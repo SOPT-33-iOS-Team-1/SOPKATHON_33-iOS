9834dce6a3bd571b6d4c522c996424aec00e20a4
HELLOHIDI <fbgmlwo123@naver.com>
2023-11-24 02:37:22 +0900

    [HOTFIX] Device 추가

--- SOPKATHON_33-iOS/SOPKATHON_33-iOS/Network/Foundation/BaseTargetType.swift
+++ SOPKATHON_33-iOS/SOPKATHON_33-iOS/Network/Foundation/BaseTargetType.swift
@@ -13,7 +13,7 @@ protocol BaseTargetType: TargetType{ }
 extension BaseTargetType{
     
     var baseURL: URL {
-        return URL(string: "")
+        return URL(string: "")!
 //        return URL(string: Config.baseURL)!
     }
     
