a4b52fc03ca919b3455d1bf1daab462f10f51c71
bangMinji98 <whoru9806@gmail.com>
2023-11-25 23:53:25 +0900

    [Feat] 탭 바 컨트롤러 구현 #15

--- SOPKATHON_33-iOS/SOPKATHON_33-iOS/Application/SceneDelegate.swift
+++ SOPKATHON_33-iOS/SOPKATHON_33-iOS/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = UIWindow(windowScene: windowScene)
         window?.overrideUserInterfaceStyle = .light
         
-        let mainViewController = ViewController()
+        let mainViewController = TabBarController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
 
         navigationController.isNavigationBarHidden = true
