ea0f5f1eefe16300f0d788f285d5bf220a9eea75
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-26 04:15:48 +0900

    [Feat] Chip뷰 구현 #26

---
+++ SOPKATHON_33-iOS/SOPKATHON_33-iOS/Presentation/Main/View/UserInfoChipView.swift
@@ -0,0 +1,56 @@
+//
+//  UserInfoChipView.swift
+//  SOPKATHON_33-iOS
+//
+//  Created by 최서연 on 11/26/23.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class UserInfoChipView: UIView {
+    
+    var labelWidth: Int = 0
+    var infoLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setUI()
+        self.setHierachy()
+        self.setLayout()
+    }
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("init(coder: NSCoder) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.do {
+            $0.backgroundColor = .white
+            $0.layer.cornerRadius = 7
+        }
+        
+        infoLabel.do {
+            $0.font = UIFont(name: "Pretendard-Regular", size: 12.adjusted)
+            $0.textColor = .systemPurple
+        }
+    }
+    
+    private func setHierachy() {
+        self.addSubview(infoLabel)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(40)
+            $0.height.equalTo(22.adjusted)
+        }
+        infoLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
